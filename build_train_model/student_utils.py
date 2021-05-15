236019adf9aab82d54041cb01062ec51a50f3cbd
HUA1846 <hua1378246@gmail.com>
2021-05-14 18:04:47 -0700

    Add files via upload

--- build_train_model/student_utils.py
+++ build_train_model/student_utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import tensorflow as tf
+import functools
 
 ####### STUDENTS FILL THIS OUT ######
 #Question 3
@@ -10,7 +11,7 @@ def reduce_dimension_ndc(df, ndc_df):
                   left_on='ndc_code', 
                   right_on='NDC_Code')
     df['generic_drug_name'] = df['Non-proprietary Name']
-    df = df.drop(['NDC_Code', 'Non-proprietary Name'], axis=1)
+    df = df.drop(['NDC_Code', 'Non-proprietary Name', 'ndc_code'], axis=1)
     return df
 
 #Question 4
@@ -25,15 +26,16 @@ def select_first_encounter(df):
 
 #Question 6
 def patient_dataset_splitter(df, patient_key='patient_nbr'):
-    '''
-    df: pandas dataframe, input dataset that will be split
-    patient_key: string, column that is the patient id
-
-    return:
-     - train: pandas dataframe,
-     - validation: pandas dataframe,
-     - test: pandas dataframe,
-    '''
+    # train 0.6, valid 0.2, test 0.2
+    
+    df = df.iloc[np.random.permutation(len(df))]
+    unique_values = df[patient_key].unique()
+    total_values = len(unique_values)
+    train_size = round(total_values * 0.6)
+    valid_size = round(total_values * 0.2)
+    train = df[df[patient_key].isin(unique_values[:train_size])].reset_index(drop=True)
+    validation = df[df[patient_key].isin(unique_values[train_size: (train_size + valid_size)])].reset_index(drop=True)
+    test = df[df[patient_key].isin(unique_values[(train_size + valid_size):])].reset_index(drop=True)
     return train, validation, test
 
 #Question 7
@@ -49,6 +51,10 @@ def create_tf_categorical_feature_cols(categorical_col_list,
     output_tf_list = []
     for c in categorical_col_list:
         vocab_file_path = os.path.join(vocab_dir,  c + "_vocab.txt")
+        category_vocab = tf.feature_column.categorical_column_with_vocabulary_file(c, vocab_file_path)
+        
+        dims = 10
+        tf_categorical_feature_column = tf.feature_column.embedding_column(category_vocab, dimension=dims)
         '''
         Which TF function allows you to read from a text file and create a categorical feature
         You can use a pattern like this below...
@@ -77,6 +83,10 @@ def create_tf_numeric_feature(col, MEAN, STD, default_value=0):
     return:
         tf_numeric_feature: tf feature column representation of the input field
     '''
+    normalizer = functools.partial(normalize_numeric_with_zscore, mean=MEAN, std=STD)
+    tf_numeric_feature = tf.feature_column.numeric_column(
+        key= col, default_value=default_value, dtype=tf.float64, normalizer_fn=normalizer)
+    
     return tf_numeric_feature
 
 #Question 9
@@ -84,8 +94,8 @@ def get_mean_std_from_preds(diabetes_yhat):
     '''
     diabetes_yhat: TF Probability prediction object
     '''
-    m = '?'
-    s = '?'
+    m = diabetes_yhat.mean()
+    s = diabetes_yhat.stddev()
     return m, s
 
 # Question 10
@@ -96,4 +106,6 @@ def get_student_binary_prediction(df, col):
     return:
         student_binary_prediction: pandas dataframe converting input to flattened numpy array and binary labels
     '''
+    student_binary_prediction = df[col].apply(lambda x: 1 if x >= 5 else 0).values
+    
     return student_binary_prediction
